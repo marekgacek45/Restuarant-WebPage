5425f2d4d72b4addcc0dc86bc1f0423d51c8e224
Marek <marekgacek45@gmail.com>
2022-06-13 12:01:05 +0200

    ended navigation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-
-
 const hamburgerBtn = document.querySelector('.hamburger')
+const mobileNav = document.querySelector('.nav__list--mobile')
+
 
 const hamburger = () => {
 	hamburgerBtn.classList.toggle('is-active')
+	mobileNav.classList.toggle('nav__list--mobile--isActive')
+	
 }
 
 hamburgerBtn.addEventListener('click', hamburger)
+
+
+
+// nav__list--mobile--isActive
