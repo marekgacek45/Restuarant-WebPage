5768956087b9a81b89585bd1542c76e4e993d879
Marek <marekgacek45@gmail.com>
2022-06-14 19:54:20 +0200

    added header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const hamburgerBtn = document.querySelector('.hamburger')
 const links = document.querySelector('.nav__links')
+const nav = document.querySelector('.nav')
 
 const hamburger = () => {
 	hamburgerBtn.classList.toggle('is-active')
 	links.classList.toggle('active')
-	
 }
+
+
 
 hamburgerBtn.addEventListener('click', hamburger)
 
 
 
-// nav__list--mobile--isActive
+
