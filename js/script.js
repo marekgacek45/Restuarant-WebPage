a74f562c30332d7251773dedb05035707d154772
Marek <marekgacek45@gmail.com>
2022-06-20 08:41:46 +0200

    finished section footer

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 const hamburgerBtn = document.querySelector('.hamburger')
 const links = document.querySelector('.nav__links')
 const nav = document.querySelector('.nav')
+const footerYear = document.querySelector('.footer__year')
 
 const hamburger = () => {
 	hamburgerBtn.classList.toggle('is-active')
 	links.classList.toggle('active')
 }
 
+const handleFooterYear = () =>{
+const date = new Date()
+const year = date.getFullYear()
+
+footerYear.innerHTML = year
+}
+
 
 
 hamburgerBtn.addEventListener('click', hamburger)
 
-
+handleFooterYear()
 
 
