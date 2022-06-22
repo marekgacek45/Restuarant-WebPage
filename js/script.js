48031126e7b35ceb50b16b6e3875a7ea549949f5
Marek <marekgacek45@gmail.com>
2022-06-22 19:16:35 +0200

    little changes in js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const hamburgerBtn = document.querySelector('.hamburger')
 const links = document.querySelector('.nav__links')
+const linksA = document.querySelectorAll('.nav__links-a')
 const nav = document.querySelector('.nav')
+const sections = document.querySelectorAll('.section')
 const footerYear = document.querySelector('.footer__year')
 
 const hamburger = () => {
 	hamburgerBtn.classList.toggle('is-active')
 	links.classList.toggle('active')
+	links.classList.toggle('show-nav')
 }
 
 const handleFooterYear = () =>{
@@ -15,10 +18,15 @@ const year = date.getFullYear()
 footerYear.innerHTML = year
 }
 
-
+const closeNav = () => {
+	linksA.forEach(linkA => {
+		linkA.addEventListener('click',()=>hamburger())
+	})
+}
 
 hamburgerBtn.addEventListener('click', hamburger)
 
 handleFooterYear()
+closeNav()
 
 
